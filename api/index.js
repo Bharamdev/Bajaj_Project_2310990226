1db152af5665831966e1d277325fd57c6a47852b
Bharamdev4 <bharamdevbansal2132004@gmail.com>
2026-02-10 12:35:17 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 // Vercel serverless entry: wraps Express app
-const fetch = require('node-fetch');
 const serverless = require('serverless-http');
 const app = require('../server');
 module.exports = serverless(app);
